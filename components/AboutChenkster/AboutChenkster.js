21b2a55d2d4a5c964d7f56859702a3daa54fdc83
Lautaro Perez Herrera <lautaroph@gmail.com>
2023-03-07 11:51:02 -0300

    add responsive styles for index

--- components/AboutChenkster/AboutChenkster.js
+++ components/AboutChenkster/AboutChenkster.js
@@ -7,7 +7,7 @@ const AboutChenkster = ({ user }) => {
     <>
       <ChenksterLogo />
       <InfoChenkster user={user} />
-      <div className="absolute bottom-0 left-0 right-0 z-0 flex items-center justify-center">
+      <div className="absolute bottom-0 left-0 right-0 z-0 flex items-center justify-center m-auto sm:left-20 md:left-40 lg:left-64 xl:left-96">
         <WorldSvg />
       </div>
     </>
