8ffa4817b096c78dccefe24c7eb89aae57d39d07
Renato <renatopiermarinih@gmail.com>
2023-03-08 12:13:51 -0300

    fix create user when register

--- utils/loginWithProvider.js
+++ utils/loginWithProvider.js
@@ -1,4 +1,5 @@
 import { uuid } from "uuidv4";
+import { uploadProfile } from "./uploadProfile";
 
 export const loginWithProvider = async (provider, supabase) => {
   const { data, error } = await supabase.auth.signInWithOAuth({
@@ -23,6 +24,16 @@ export const loginWithProvider = async (provider, supabase) => {
       username: uuid()
     },
   });
+
+  await uploadProfile({
+    role: data.user.user_metadata.role,
+    first_name: data.user.user_metadata.first_name,
+    last_name: data.user.user_metadata.last_name,
+    username: data.user.user_metadata.username,
+    description: '',
+    location: [],
+    language: []
+  }, 'https://res.cloudinary.com/dv1ksnrvk/image/upload/v1677080765/samples/userImg_oiynrs.png',data.user.id,supabase)
 
   return { data, error };
 };
--- utils/registerWIthEmail.js
+++ utils/registerWIthEmail.js
@@ -1,4 +1,5 @@
 import { uuid } from "uuidv4";
+import { uploadProfile } from "./uploadProfile";
 
 export const registerWithEmail = async (email, password, supabase) => {
   const { data, error } = await supabase.auth.signUp({
@@ -16,6 +17,16 @@ export const registerWithEmail = async (email, password, supabase) => {
       },
     },
   });
+
+  await uploadProfile({
+    role: data.user.user_metadata.role,
+    first_name: data.user.user_metadata.first_name,
+    last_name: data.user.user_metadata.last_name,
+    username: data.user.user_metadata.username,
+    description: '',
+    location: [],
+    language: []
+  }, 'https://res.cloudinary.com/dv1ksnrvk/image/upload/v1677080765/samples/userImg_oiynrs.png',data.user.id,supabase)
 
   return { data, error };
 };
